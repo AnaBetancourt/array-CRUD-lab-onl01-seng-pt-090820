780d39eebd1335ff6f25b3a1378a104c9fbd71a6
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 04:53:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,3 +2,41 @@ def create_an_empty_array
   array = []
 end
 
+def create_an_array
+  array = ["item 1", "item2", "item3", "item4"]
+end
+
+def add_element_to_end_of_array(array, element)
+  array = ["item 1", "item2", "item3", "item4"]
+  arrayarray << "item5"
+end
+
+def add_element_to_start_of_array(array, element)
+  array = ["item 1", "item2", "item3", "item4"]
+  arrayy.unshift("item0")
+end
+
+def remove_element_from_end_of_array(array)
+  array = ["item 1", "item2", "item3", "item4"]
+  array.pop
+end
+
+def remove_element_from_start_of_array(array)
+  array = ["item 1", "item2", "item3", "item4"]
+  array.shift
+end
+
+def retrieve_element_from_index(array, index_number)
+  array = ["item 1", "item2", "item3", "item4"]
+  array[1]
+end
+
+def retrieve_first_element_from_array(array)
+  array = ["item 1", "item2", "item3", "item4"]
+  array[0]
+end
+
+def retrieve_last_element_from_array(array)
+  array = ["item1", "item2", "item3", "item4"]
+  array[-1]
+end
