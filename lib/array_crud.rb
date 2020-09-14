9cc2adcc669bb43da4009b981599b2dfe734befa
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 04:47:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ["item1", "item2", "item3", "item4"]
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
