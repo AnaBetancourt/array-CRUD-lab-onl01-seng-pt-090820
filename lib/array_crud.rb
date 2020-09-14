72f748dec9343799b8b6f82afd7478022c1b72f9
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 04:58:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["item 1", "item2", "item3", "item4"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.shift
 end
 
