0d28568a8246a0bad33ebb1a6a819fbd76a53b0f
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 04:50:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["item1", "item2", "item3", "item4"]
-  add_element_to_end_of_array.unshift("item0")
+  array = ["item 1", "item2", "item3", "item4"]
+  arrayy.unshift("item0")
 end
 
 def remove_element_from_end_of_array(array)
