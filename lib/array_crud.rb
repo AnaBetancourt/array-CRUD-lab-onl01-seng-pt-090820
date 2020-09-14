78255b11e285587f9fb1d4edfbd3be859dc79221
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 04:57:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["item 1", "item2", "item3", "item4"]
+  array = ["I", "am", "really", "learning"]
   array.unshift("wow")
 end
 
